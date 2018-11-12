c0259ada13979067b4ec34f7ff4dd2a4e747eb72
Nalini Ganapati <nganapati@gmail.com>
2018-11-12 13:47:02 -0800

    Use org.genomicsdb published jars

--- run_checks.sh
+++ run_checks.sh
@@ -1,17 +1,26 @@
 #!/bin/bash 
 osname=`uname -s`
-jar xf genomicsdb-${GENOMICSDB_VERSION}-jar-with-dependencies.jar libtiledbgenomicsdb.so
-jar xf genomicsdb-${GENOMICSDB_VERSION}-jar-with-dependencies.jar libtiledbgenomicsdb.dylib
-if [ "$osname" == "Darwin" ];
-then
-    LIBRARY_SUFFIX=dylib
-    otool -L libtiledbgenomicsdb.${LIBRARY_SUFFIX}
-    md5 -r libtiledbgenomicsdb.${LIBRARY_SUFFIX}
+if [ "$osname" == "Linux" ]; then
+	LIBRARY_SUFFIX=so
+	jar xf genomicsdb-${GENOMICSDB_VERSION}-jar-with-dependencies.jar libtiledbgenomicsdb.${LIBRARY_SUFFIX}
+	if [[ -f libtiledbgenomicsdb.${LIBRARY_SUFFIX} ]]; then
+		ldd libtiledbgenomicsdb.${LIBRARY_SUFFIX}
+		md5sum libtiledbgenomicsdb.${LIBRARY_SUFFIX}
+	else
+		echo "Could not find libtiledbgenomicsdb.${LIBRARY_SUFFIX}"
+		exit 1
+	fi
+elif [ "$osname" == "Darwin" ]; then
+	LIBRARY_SUFFIX=dylib
+	jar xf genomicsdb-${GENOMICSDB_VERSION}-jar-with-dependencies.jar libtiledbgenomicsdb.{LIBRARY_SUFFIX}
+	if [[ -f libtiledbgenomicsdb.${LIBRARY_SUFFIX} ]]; then
+		otool -L libtiledbgenomicsdb.${LIBRARY_SUFFIX}
+		md5 -r libtiledbgenomicsdb.${LIBRARY_SUFFIX}
+	else
+		echo "Could not find libtiledbgenomicsdb.${LIBRARY_SUFFIX}"
+		exit 1
+	fi
 else
-    LIBRARY_SUFFIX=so
-    ldd libtiledbgenomicsdb.${LIBRARY_SUFFIX}
-    md5sum libtiledbgenomicsdb.${LIBRARY_SUFFIX}
+	echo "Platform $osname is not supported"
+	exit 1
 fi
-#rm -f libtiledbgenomicsdb.${LIBRARY_SUFFIX}
-#jar xf genomicsdb-${GENOMICSDB_VERSION}.jar libtiledbgenomicsdb.${LIBRARY_SUFFIX}
-#md5sum libtiledbgenomicsdb.${LIBRARY_SUFFIX}
