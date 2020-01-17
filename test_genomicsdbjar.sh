b02d25c074f267b9717526316b8b2b8b992f2703
nalinigans <nalini@omicsautomation.com>
2020-01-17 15:30:36 -0800

    Check 1.2.1 snapshot

--- test_genomicsdbjar.sh
+++ test_genomicsdbjar.sh
@@ -8,8 +8,8 @@ IS_SNAPSHOT=false
 rm -fr /tmp/ws
 
 if [ ${IS_SNAPSHOT} == true ]; then
-		export GENOMICSDB_VERSION=1.2.0-20191127.233149-2
-		export GENOMICSB_REPOSITORY_VERSION=1.2.0-SNAPSHOT
+		export GENOMICSDB_VERSION=1.2.1-20200117.231140-1
+		export GENOMICSB_REPOSITORY_VERSION=1.2.1-SNAPSHOT
 		export MAVEN_REPOSITORY=https://oss.sonatype.org/content/repositories/snapshots/org/genomicsdb/genomicsdb/${GENOMICSB_REPOSITORY_VERSION}
 else
 	export GENOMICSDB_VERSION=1.2.0
