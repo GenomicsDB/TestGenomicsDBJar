a9ade59f0f0a1d2d61c9d6486568244223a9f05f
nalinigans <nalini@omicsautomation.com>
2020-05-29 09:40:00 -0700

    Try with the snapshot from develop stream for debugging

--- test_genomicsdbjar.sh
+++ test_genomicsdbjar.sh
@@ -8,7 +8,7 @@ IS_SNAPSHOT=true
 rm -fr /tmp/ws
 
 if [ ${IS_SNAPSHOT} == true ]; then
-		export GENOMICSDB_VERSION=1.2.3-20200529.151105-1
+		export GENOMICSDB_VERSION=1.2.3-20200529.162659-2
 		export GENOMICSB_REPOSITORY_VERSION=1.2.3-SNAPSHOT
 		export MAVEN_REPOSITORY=https://oss.sonatype.org/content/repositories/snapshots/org/genomicsdb/genomicsdb/${GENOMICSB_REPOSITORY_VERSION}
 else
