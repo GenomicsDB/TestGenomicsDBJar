7ae2c71ebefd0a27e92fc433698cc365048d22c0
eneskuluk <54481799+eneskuluk@users.noreply.github.com>
2023-03-30 15:01:16 -0700

    fix by removing line

--- run_checks.sh
+++ run_checks.sh
@@ -18,7 +18,6 @@ if [[ ! -f genomicsdb-${GENOMICSDB_VERSION}.jar ]]; then
   echo "Could not find genomicsdb-${GENOMICSDB_VERSION}.jar"
   exit 1
 fi
-file_size_kb=`du -k genomicsdb-${GENOMICSDB_VERSION}-allinone-spark.jar | cut -f1`
 if [ $file_size_kb == 0 ]; then
   echo "genomicsdb-${GENOMICSDB_VERSION}-allinone-spark.jar or genomicsdb-${GENOMICSDB_VERSION}-allinone.jar has no contents"
   exit 1
