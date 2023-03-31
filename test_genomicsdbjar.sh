dcad96a7c17eb2730315049d90a1d3c083f28246
nalinigans <nalini@omicsautomation.com>
2023-03-31 15:58:59 +0530

    Merge changes from ml_genomicsdb_release

--- test_genomicsdbjar.sh
+++ test_genomicsdbjar.sh
@@ -7,28 +7,70 @@ IS_SNAPSHOT=false
 
 rm -fr /tmp/ws
 
-if [ ${IS_SNAPSHOT} == true ]; then
-		export GENOMICSDB_VERSION=1.4.3-20211216.233338-1	
-		export GENOMICSB_REPOSITORY_VERSION=1.4.3-SNAPSHOT
-		export MAVEN_REPOSITORY=https://oss.sonatype.org/content/repositories/snapshots/org/genomicsdb/genomicsdb/${GENOMICSB_REPOSITORY_VERSION}
-else
-	export GENOMICSDB_VERSION=1.4.3
-	#export MAVEN_REPOSITORY=https://oss.sonatype.org/content/repositories/staging/org/genomicsdb/genomicsdb/${GENOMICSDB_VERSION}
-  export MAVEN_REPOSITORY=https://repo1.maven.org/maven2/org/genomicsdb/genomicsdb/${GENOMICSDB_VERSION}
+# if GENOMICSDB_VERSION is already defined we expect the jar to already exist
+if [ -z "$GENOMICSDB_VERSION" ]; then
+    if [ ${IS_SNAPSHOT} == true ]; then
+        export GENOMICSDB_VERSION=1.4.3-20211216.233338-1	
+        export GENOMICSB_REPOSITORY_VERSION=1.4.3-SNAPSHOT
+        export MAVEN_REPOSITORY=https://oss.sonatype.org/content/repositories/snapshots/org/genomicsdb/genomicsdb/${GENOMICSB_REPOSITORY_VERSION}
+    else
+        export GENOMICSDB_VERSION=1.4.3
+        #export MAVEN_REPOSITORY=https://oss.sonatype.org/content/repositories/staging/org/genomicsdb/genomicsdb/${GENOMICSDB_VERSION}
+        export MAVEN_REPOSITORY=https://repo1.maven.org/maven2/org/genomicsdb/genomicsdb/${GENOMICSDB_VERSION}
+    fi
+
+    ####
+    # Leave rest of the script alone
+    ####
+    echo "Using MAVEN_REPOSITORY=$MAVEN_REPOSITORY"
+
+    curl -O ${MAVEN_REPOSITORY}/genomicsdb-${GENOMICSDB_VERSION}-allinone.jar
+    curl -O ${MAVEN_REPOSITORY}/genomicsdb-${GENOMICSDB_VERSION}.jar
 fi
 
-####
-# Leave rest of the script alone
-####
-echo "Using MAVEN_REPOSITORY=$MAVEN_REPOSITORY"
+export CLASSPATH=genomicsdb-${GENOMICSDB_VERSION}-allinone.jar:.
 
-curl -O ${MAVEN_REPOSITORY}/genomicsdb-${GENOMICSDB_VERSION}-allinone.jar
-curl -O ${MAVEN_REPOSITORY}/genomicsdb-${GENOMICSDB_VERSION}.jar
-
-GENOMICSDB_TAG=v$GENOMICSDB_VERSION ./run_checks.sh
+./run_checks.sh
 if [[ $? -ne 0 ]]; then
     echo "run_checks FAILED"
     exit 1
 fi
+
+echo
+echo "Basic GenomicsDBVersion check..."
+javac GenomicsDBGetVersion.java && java GenomicsDBGetVersion
+if [[ $? -ne 0 ]]; then
+    echo "GenomicsDBGetVersion FAILED"
+    exit 1
+fi
+echo "Done"
+echo
+
+rm -fr GenomicsDB
+git clone https://github.com/GenomicsDB/GenomicsDB.git
+
+echo "Compiling Test Classes..."
+rm -fr *.class
+javac -d . GenomicsDB/example/java/*.java &&
+echo "Done" &&
+echo &&
+
+echo "Starting ETL..." &&
+java TestGenomicsDBImporterWithMergedVCFHeader -L 1:1-100000 -w /tmp/ws -A test0 GenomicsDB/tests/inputs/vcfs/t0.vcf.gz GenomicsDB/tests/inputs/vcfs/t1.vcf.gz GenomicsDB/tests/inputs/vcfs/t2.vcf.gz --vidmap-output /tmp/vid_pb.json --callset-output /tmp/callset_pb.json &&
+echo "ETL SUCCESS" &&
+echo &&
+
+echo "Starting query..." &&
+java TestGenomicsDB --query -l loader.json query.json &&
+rm -fr /tmp/ws &&
+echo "Query SUCCESS" &&
+echo
+
+if [[ $? -ne 0 ]]; then
+    echo "TestGenomicsDBJAR FAILED"
+    exit 1
+fi
+
+
 
 
